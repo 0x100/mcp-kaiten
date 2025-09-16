48cd15c291fa47ea580810e308d67eb551bb57a1
Ilya Lysenko <lysenko.ilya@gmail.com>
2025-09-16 23:41:15 +0300

    Initialize Spring App

---
+++ src/main/java/ru/coder1/mcp/kaiten/McpKaitenApplication.java
@@ -0,0 +1,13 @@
+package ru.coder1.mcp.kaiten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpKaitenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpKaitenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/coder1/mcp/kaiten/McpKaitenApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coder1.mcp.kaiten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpKaitenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
